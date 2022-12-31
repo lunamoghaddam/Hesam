5c75f03f8b42e7b4b0fb8191d542f7c35e3a3f12
Ali Haqiqi <aliz@foresightanalytics.ca>
2023-01-01 02:37:06 +0330

    add structure in services
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

---
+++ src/routes/services/+page.svelte
@@ -0,0 +1,40 @@
+<script lan="ts">
+  const items = [
+    {
+      title: 'first t',
+      subtitle: 'first s',
+    },
+    {
+      title: 'second t',
+      subtitle: 'second s',
+    },
+    {
+      title: 'third t',
+      subtitle: 'third s',
+    }
+  ]
+</script>
+
+<div class="service-container">
+{#each items as item}
+  <div class="row">
+    <h3>{ item.title }</h3>
+    <span>{ item.subtitle }</span>
+  </div>
+{/each}
+</div>
+
+<style lang="scss">
+  .service-container {
+    @extend .col;
+
+    .row {
+      background-color: $on-surface;
+      margin-bottom: 1rem;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+</style>
